2fff2afbd4247e86ff54ad8c71616b38f04decaa
Alex Fernandes Romera <af_romera@hotmail.com>
2018-12-06 23:58:26 -0200

    revisao 3

--- ESNext/revisao3.js
+++ ESNext/revisao3.js
@@ -1,1 +1,34 @@
-//removendo varias linhas
+// ES8 Object.values e Object.entries
+
+const obj = {a: 1, b: 2, c: 3}
+
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+// Melhorias na notação Literal
+const nome = 'alex'
+
+const pessoa = {
+    nome,
+    idade: 28,
+    ola(){
+        return ' Oi Pessoal'
+    }
+}
+
+console.log(`${pessoa.nome} disse: ${pessoa.ola()}`)
+
+//classe
+class Animal {
+    peso(){
+        return 30
+    }
+}
+class Cachorro extends Animal {
+    falar(){
+        return 'au au au'
+    }
+}
+
+const dog = new Cachorro()
+console.log(dog.peso(),dog.falar())
