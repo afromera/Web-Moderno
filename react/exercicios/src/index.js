ef5abfa2b922829cd87bfacc404907e2afd962cc
Alex Fernandes Romera <af_romera@hotmail.com>
2018-12-22 12:19:37 -0200

    React Multiplos Componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia nome='Alex Fernandes' idade={28}/>, document.getElementById('root'))
+import Mult, { BoaNoite } from './componentes/Multiplos'
+
+ReactDOM.render(
+    <div>
+        <Mult.BoaTarde nome='Alex' />
+        <BoaNoite nome='Caroline' />
+    </div>
+ , document.getElementById('root'))
