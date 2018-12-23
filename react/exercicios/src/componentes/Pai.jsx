57169f9cbc15675cb3488c6aa67d12fc9cbc5c85
Alex Fernandes Romera <af_romera@hotmail.com>
2018-12-23 21:40:39 -0200

    Componentes Pai e Filho 2

--- react/exercicios/src/componentes/Pai.jsx
+++ react/exercicios/src/componentes/Pai.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Filho from './Filho'
+import { childrenWithProps } from '../utils/utils'
+
 
 export default props => 
     <div>
         <h1>{props.nome} {props.sobrenome}</h1>
         <h3>Filhos</h3>
         <ul>
-            <Filho nome='Grazielle' sobrenome={props.sobrenome}/>
-            <Filho {...props} sobrenome={props.sobrenome + ' Jr'}/>
-            <Filho {...props} nome='Kaue' />
+            {childrenWithProps(props)}
         </ul>
     </div>
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -8,11 +8,15 @@ import ReactDOM from 'react-dom'
 // import Saudacao from './componentes/Saudacao'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
 
     <Pai nome='Alex' sobrenome='Romera'>
         {/* Como passar os filhor por aqui? */}
+        <Filho nome='Grazielle' sobrenome='Romera' />
+        <Filho nome='Alex' sobrenome='Romera' />
+        <Filho nome='Kaue' sobrenome='Romera' />
     </Pai>
 
 ,document.getElementById('root'))
