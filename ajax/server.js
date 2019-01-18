9db4398b57f9cbf62b6434dd31005bd5da226c30
Alex Fernandes Romera <af_romera@hotmail.com>
2019-01-17 22:40:56 -0200

    ajax - xmlHttpRequest - retornando estados.

--- ajax/server.js
+++ ajax/server.js
@@ -5,6 +5,4 @@ const app = express()
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-
-app.get('/teste', (req, res) => res.send('ok'))
 app.listen(8080, () => console.log('Executando...'))
